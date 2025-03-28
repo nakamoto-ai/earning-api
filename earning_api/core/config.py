aaab13174dca6645178ec43fb4eea744f45e769d
Edward Ciccone <edward@localhost.localdomain>
2025-03-28 13:07:37 -0500

    support cors origins via pydantic

--- earning_api/core/config.py
+++ earning_api/core/config.py
@@ -1,7 +1,8 @@
 import sys
+from typing import List
 
 from dotenv import load_dotenv
-from pydantic import field_validator
+from pydantic import Field, field_validator
 from pydantic_settings import BaseSettings
 
 load_dotenv()
@@ -12,6 +13,9 @@ class Settings(BaseSettings):
     LOG_LEVEL: str = "INFO"
     PORT: int = 8000
     TOKEN: str = ""
+    CORS_ORIGINS: List[str] = Field(
+        default=["http://localhost:3000"],
+        description="List of allowed CORS origins.")
 
     @field_validator("TOKEN", mode="before")
     def validate_token(cls, v):
@@ -19,6 +23,15 @@ class Settings(BaseSettings):
             print("ERROR: TOKEN is not set in the environment or .env file.")
             sys.exit(1)
         return v
+
+    @field_validator("CORS_ORIGINS", mode="before")
+    def parse_cors_origins(cls, v):
+        if isinstance(v, str):
+            return [origin.strip() for origin in v.split(",")]
+        elif isinstance(v, list):
+            return v
+        raise ValueError(
+            "CORS_ORIGIN must be a comma-seperated string or list")
 
     class Config:
         env_file = ".env"
--- earning_api/main.py
+++ earning_api/main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from earning_api.api import api_v1_router
-from earning_api.core.logging import setup_logging
 from earning_api.core.config import settings
+from earning_api.core.logging import setup_logging
 
 app = FastAPI(
     title="People Earnings API",
     description="API to manage people and their earnings",
     version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 setup_logging()
